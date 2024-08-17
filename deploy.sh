85a3e852ac672f117ce61542e5a662818f4627f2
alkor2 <alkor22@student.sdu.dk>
2024-08-17 12:27:27 +0000

    Modified deploy.sh to include redis

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,15 @@ echo "${KUBECONFIG}" | base64 -d > kubeconfig
 
 export KUBECONFIG=kubeconfig
 
-kubectl apply -f frontend/deployment.yaml
-kubectl apply -f frontend/service.yaml
+kubectl apply -f k8s/deploymentfrontend.yaml
+kubectl apply -f k8s/servicefrontend.yaml
+kubectl apply -f k8s/configfrontend.yaml
 
-kubectl apply -f backend/deployment.yaml
-kubectl apply -f backend/service.yaml
+kubectl apply -f k8s/deploymentbackend.yaml
+kubectl apply -f k8s/servicebackend.yaml
+kubectl apply -f k8s/configbackend.yaml
+
+kubectl apply -f k8s/deploymentredis.yaml
+kubectl apply -f k8s/serviceredis.yaml
 
 rm kubeconfig
