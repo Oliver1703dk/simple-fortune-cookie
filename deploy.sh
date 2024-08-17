1355dd5247c6a86ab9d344a42df9b467f53ce4df
alkor2 <alkor22@student.sdu.dk>
2024-08-17 12:43:58 +0000

    Modified deploy.sh to retrive data from secret

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@ echo "${KUBECONFIG}" | base64 -d > kubeconfig
 
 export KUBECONFIG=kubeconfig
 
-kubectl apply -f k8s/deploymentfrontend.yaml
-kubectl apply -f k8s/servicefrontend.yaml
-kubectl apply -f k8s/configfrontend.yaml
+kubectl --kubeconfig KUBECONFIG apply -f k8s/deploymentfrontend.yaml
+kubectl --kubeconfig KUBECONFIG apply -f k8s/servicefrontend.yaml
+kubectl --kubeconfig KUBECONFIG apply -f k8s/configfrontend.yaml
 
-kubectl apply -f k8s/deploymentbackend.yaml
-kubectl apply -f k8s/servicebackend.yaml
-kubectl apply -f k8s/configbackend.yaml
+kubectl --kubeconfig KUBECONFIG apply -f k8s/deploymentbackend.yaml
+kubectl --kubeconfig KUBECONFIG apply -f k8s/servicebackend.yaml
+kubectl --kubeconfig KUBECONFIG apply -f k8s/configbackend.yaml
 
-kubectl apply -f k8s/deploymentredis.yaml
-kubectl apply -f k8s/serviceredis.yaml
+kubectl --kubeconfig KUBECONFIG apply -f k8s/deploymentredis.yaml
+kubectl --kubeconfig KUBECONFIG apply -f k8s/serviceredis.yaml
 
 rm kubeconfig
